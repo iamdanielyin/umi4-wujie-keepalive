4ef581e5c29b01794ee94d7ebbf2390281d4fa84
Daniel Yin <iamdanielyin@gmail.com>
2023-08-03 22:43:09 +0800

    feat: 初步集成wujie

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import WujieReact from 'wujie-react';
 import { request } from '@umijs/max';
 
 export async function getInitialState() {
@@ -5,6 +6,14 @@ export async function getInitialState() {
     const menuMap: any = {}
     for (const item of menus) {
         menuMap[item.id] = item
+        // 预加载所有子应用
+        if (item?.subAppCode) {
+            WujieReact.preloadApp({
+                name: item.subAppCode,
+                url: item.subAppHost,
+                exec: true
+            } as any)
+        }
     }
     return { menus, menuMap };
 }
